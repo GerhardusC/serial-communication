7f8869ba4a3d81f4c2c95507d86aa8fd441f12fd
Gerhardus <hardus.cronje@gmail.com>
2025-08-23 23:58:34 +0200

    feat: convert int to measurement

--- host_device/main/host_device.c
+++ host_device/main/host_device.c
@@ -18,6 +18,36 @@ void wait_us_blocking(uint32_t micros_to_wait) {
     while(micros_now_plus_delay > esp_timer_get_time()){}
 }
 
+struct Temp_reading {
+    uint16_t temp_sig;
+    uint16_t temp_dec;
+    uint16_t hum_sig;
+    uint16_t hum_dec;
+};
+
+void set_measurement_from_int(int msg, struct Temp_reading *measurement) {
+	int top =	0b11111111 << 24;
+	int top_mid =	0b11111111 << 16;
+	int bot_mid =	0b11111111 << 8;
+	int bot =	0b11111111;
+
+	measurement->temp_sig = (top & msg) >> 24;
+	measurement->temp_dec = (top_mid & msg) >> 16;
+	measurement->hum_sig = (bot_mid & msg) >> 8;
+	measurement->hum_dec = bot & msg;
+}
+
+void print_measurement(struct Temp_reading *measurement) {
+	ESP_LOGI(
+		"MEASUREMENT",
+		"\nTemp: %d,%d\nHum:  %d,%d",
+		measurement->temp_sig,
+		measurement->temp_dec,
+		measurement->hum_sig,
+		measurement->hum_dec
+	);
+}
+
 int recv_message() {
 	int msg = 0;
 
@@ -50,8 +80,12 @@ void app_main(void) {
 	gpio_set_direction(MISO, GPIO_MODE_INPUT);
 	gpio_set_level(MISO, 1);
 
+	struct Temp_reading *measurement = malloc(sizeof(struct Temp_reading));
+
 	while (1) {
 		vTaskDelay(200);
-		recv_message();
+		int msg = recv_message();
+		set_measurement_from_int(msg, measurement);
+		print_measurement(measurement);
 	}
 }
